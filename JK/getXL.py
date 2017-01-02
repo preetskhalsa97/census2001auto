3bd2db8f9a884c86b80fea1bdab537a0c7d633ad
preetskhalsa97 <preetskhalsa97@gmail.com>
2017-01-03 03:50:41 +0530

    Update getXL.py

--- JK/getXL.py
+++ JK/getXL.py
@@ -3,7 +3,7 @@ c = pdftables_api.Client('80w1fpv0089g')
 
 for i in range(1,15):
 	if (i<10):
-		string='0'+str(i)
+		string='0'+str(i) 
 	else:
 		string=str(i)
 	c.xlsx('district'+string+'.pdf', 'output'+string+'.xlsx')
