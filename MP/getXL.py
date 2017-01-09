75ed2f2867f64f792fe9cfa1d034c891f70713af
psk <preetskhalsa97@gmail.com>
2017-01-10 01:09:32 +0530

    PDFsobtained

--- MP/getXL.py
+++ MP/getXL.py
@@ -2,8 +2,6 @@ import pdftables_api
 c = pdftables_api.Client('emrssrlm10zx')
 
 for i in range(1,46):
-	if (i==39 or i==44):
-		continue
 	if (i<10):
 		string='0'+str(i) 
 	else:
---
+++ Uttranchal/getPDF.py
@@ -0,0 +1,34 @@
+import urllib 
+
+def downloadPDF(state_code):
+
+	#getting the string fpr state code
+	if (state_code<10):
+		string_state='0'+str(state_code)
+	else:
+		string_state=str(state_code)
+
+	i=1
+
+	while True:
+		#getting the string for district code 
+		if (i<10):
+			string_district='0'+str(i)
+		else:
+			string_district=str(i)
+
+		try:
+			urllib.urlretrieve ("http://censusindia.gov.in/Dist_File/datasheet-"+string_state+string_district+".pdf","district"+string_district+".pdf")
+		except:
+			break
+		i=i+1
+
+#Uttraanchal
+code=5
+downloadPDF(code)
+
+
+
+
+
+
